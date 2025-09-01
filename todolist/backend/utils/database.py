70f78b4f35ed667c5771bde833c0311162aa90d4
Sahran <82019509+SockYeh@users.noreply.github.com>
2025-09-01 19:05:17 +0530

    feat[db]: create users db schema

--- todolist/backend/utils/database.py
+++ todolist/backend/utils/database.py
@@ -10,10 +10,44 @@ client = None
 
 async def open_db() -> None:
     """Open a connection to the MongoDB database."""
-    global client  # noqa: PLW0603
+    global client, users_db  # noqa: PLW0603
     client = AsyncIOMotorClient(connection_str)
+    users_db = client["users"]
 
 
 async def close_db() -> None:
     """Close the connection to the MongoDB database."""
     client.close()
+
+
+async def create_user_db() -> None:
+    """Create the user database."""
+    await client.drop_database("users")
+
+    auth_validator = {
+        "$jsonSchema": {
+            "bsonType": "object",
+            "required": ["username", "email", "password"],
+            "properties": {
+                "username": {
+                    "bsonType": "string",
+                    "description": "must be a string. Spotify username of the user",
+                },
+                "email": {
+                    "bsonType": "string",
+                    "description": "must be a string. Email of the user",
+                },
+                "password": {
+                    "bsonType": "string",
+                    "description": "must be a string. Password of the user",
+                },
+            },
+        },
+    }
+
+    await users_db.create_collection("auth_details")
+
+    await users_db.command("collMod", "auth_details", validator=auth_validator)
+
+    await users_db.auth_details.create_index("username", unique=True)
+    await users_db.auth_details.create_index("email", unique=True)
