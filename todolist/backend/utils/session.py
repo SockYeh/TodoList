c325dd6037cda20a25f206a5818790d8b219848f
Sahran <82019509+SockYeh@users.noreply.github.com>
2025-09-03 10:51:23 +0530

    fix(session): correct session key from "user_id" to "userid"

--- todolist/backend/utils/session.py
+++ todolist/backend/utils/session.py
@@ -4,6 +4,6 @@ from fastapi import HTTPException, Request
 async def validate_session(request: Request) -> None:
     """Validate the session of the user."""
     try:
-        request.session["user_id"]
+        request.session["userid"]
     except KeyError as e:
         raise HTTPException(status_code=401, detail="Invalid Session!") from e
