85a14d5ba50421c43104fdc9c8efff14948a170c
Sahran <82019509+SockYeh@users.noreply.github.com>
2025-09-03 08:12:12 +0530

    feat[tasks]: add task payload models

---
+++ todolist/backend/schemas/tasks.py
@@ -0,0 +1,22 @@
+from time import time
+
+from pydantic import BaseModel
+
+
+class TaskCreate(BaseModel):
+    """Schema for creating a new task."""
+
+    id: int
+    title: str
+    description: str
+    completed: bool
+    created_at: float = time()
+
+
+class TaskUpdate(BaseModel):
+    """Schema for updating an existing task."""
+
+    id: int
+    title: str | None = None
+    description: str | None = None
+    completed: bool | None = None
