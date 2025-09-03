304f469a32b8f68fa22bc605fc98071be2b4453e
Sahran <82019509+SockYeh@users.noreply.github.com>
2025-09-03 13:09:22 +0530

    feat: implement login and registration pages

--- todolist/frontend/frontend.py
+++ todolist/frontend/frontend.py
@@ -1,5 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter(prefix="", tags=["frontend"])
 templates = Jinja2Templates(directory=r"todolist\frontend")
+
+
+@router.get("/login")
+async def root(request: Request) -> HTMLResponse:
+    """Render the login page."""
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@router.get("/register")
+async def register(request: Request) -> HTMLResponse:
+    """Render the registration page."""
+    return templates.TemplateResponse("register.html", {"request": request})
--- todolist/main.py
+++ todolist/main.py
@@ -8,6 +8,7 @@ from starlette.middleware.sessions import SessionMiddleware
 from todolist.backend.routers import auth, tasks
 from todolist.backend.utils.config import env
 from todolist.backend.utils.database import close_db, open_db
+from todolist.frontend import frontend
 
 
 @asynccontextmanager
@@ -31,6 +32,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-routers = [auth.router, tasks.router]
+routers = [auth.router, tasks.router, frontend.router]
 for router in routers:
     app.include_router(router)
